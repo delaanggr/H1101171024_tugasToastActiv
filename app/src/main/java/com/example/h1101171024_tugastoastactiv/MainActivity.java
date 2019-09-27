0c2357180ae9146c85f7eb2103a6c88c9a57505a
dela <delaanggr28@gmail.com>
2019-09-27 10:29:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/h1101171024_tugastoastactiv/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.h1101171024_tugastoastactiv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 24;
+    private TextView mShowCount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.count_up);
+        mShowCount = (TextView) findViewById(R.id.count_down);
+    }
+
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void countDown(View view) {
+        mCount--;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
